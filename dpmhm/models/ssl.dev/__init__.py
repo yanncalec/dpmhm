0478c18642828ba87df42025b52c30620f7bd220
Han Wang <han.wang.fr@gmail.com>
2023-03-28 10:19:57 +0200

    Refactoring out wip ssl models.

---
+++ dpmhm/models/ssl.dev/__init__.py
@@ -0,0 +1,9 @@
+# Self-supervised learning models.
+
+from ..ssl.byol import *
+from ..ssl.cpc_v1 import *
+from ..ssl.moco import *
+from ..ssl.simclr_v1 import *
+from .simsiam import *
+from .swav import *
+from .barlow_twins import *
