3eb64825254b79257bd8f20dce80868fe534f34b
Han Wang <han.wang.fr@gmail.com>
2023-04-20 15:46:52 +0200

    GitHub actions deployment.

--- dpmhm/cli.py
+++ dpmhm/cli.py
@@ -2,6 +2,8 @@ import click
 
 @click.group()
 def dpmhm():
+    """CLI for DPMHM.
+    """
     pass
 
 @click.command()
@@ -18,5 +20,16 @@ def install(ds:str, *, data_dir:str, download_dir:str, extract_dir:str, manual_d
     dl_kwargs = {'max_simultaneous_downloads': max_sim_dl}
 
     return datasets.install(ds, data_dir=data_dir, download_dir=download_dir, extract_dir=extract_dir, manual_dir=manual_dir, dl_kwargs=dl_kwargs)
+
+@click.command()
+@click.argument('ds', default='')
+def info(ds:str):
+    from . import datasets
+    if ds == '':
+        print(datasets.get_dataset_list())
+    else:
+        print(datasets.get_info(ds))
+
+# dpmhm.add_command(info)
 
 dpmhm.add_command(install)
